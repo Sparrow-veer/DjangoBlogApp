22c7433b9d7cff34784f7748e4b0764f436d9b4e
prudhvireddy <k.prudhvireddy1996@gmail.com>
2022-02-09 11:05:36 +0530

    initial full blog commit

---
+++ blog/views.py
@@ -0,0 +1,91 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
+from django.contrib.auth.models import User
+from . import models
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+# Create your views here.
+import logging
+
+logging.basicConfig(filename='logfile.txt',level=logging.DEBUG)
+posts=[
+    {'author':'Shiv',
+     'title':'the Creator',
+     'content':'first content',
+     'date_posted':'20-09-1998'},
+    {'author':'Laxman',
+     'title':'the Brother',
+     'content':'second content',
+     'date_posted':'10-11-1994'}
+]
+def home(request):
+    content={
+        'posts':models.Post.objects.all(),
+        'title':'Authors Blog'
+    }
+    return render(request,'blog/home.html',content)
+
+def about(request):
+    return render(request,'blog/about.html')
+
+
+
+
+class PostListView(ListView):
+    model=models.Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 5
+
+
+class PostDetailView(DetailView):
+    model=models.Post
+
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model=models.Post
+    fields=['title','content',]
+
+    def form_valid(self, form):
+        logging.debug(self.request.user)
+        logging.debug(self.request.user.email)
+        form.instance.author=self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model=models.Post
+    fields=['title','content',]
+
+    def form_valid(self, form):
+        form.instance.author=self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post=self.get_object()
+        if(self.request.user==post.author):
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model=models.Post
+    success_url = '/blog/'
+    def test_func(self):
+        post=self.get_object()
+        if(self.request.user==post.author):
+            return True
+        return False
+
+
+class UserPostListView(ListView):
+    model=models.Post
+    template_name = 'blog/user_post.html'
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user=get_object_or_404(User,username=self.kwargs.get('username'))
+        return models.Post.objects.filter(author=user).order_by('-date_posted')
+
---
+++ users/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render,redirect
+# from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.views import View
+from .forms import UserRegisterForm,UserUpdateForm,ProfileUpdateForm
+
+# Create your views here.
+
+# def register(request):
+#     if(request.method=='post'):
+#         form=UserCreationForm(request.POST)
+#         if(not form.is_valid()):
+#             username=form.cleaned_data.get('username')
+#             print(username)
+#             messages.success(request, f'Account created for {username}')
+#             return redirect('blog-home')
+#
+#     else:
+#         form=UserCreationForm()
+#     return render(request,'users/register.html',{'form':form})
+
+
+
+class Validation(View):
+    def get(self,request,*args,**kwargs):
+         form=UserRegisterForm()
+         return render(request,'users/register.html',{'form':form})
+
+    def post(self,request,*args,**kwargs):
+
+        if request.method == "POST":
+            form = UserRegisterForm(request.POST)
+            if form.is_valid():
+                form.save()
+                username = form.cleaned_data.get('username')
+                print(username)
+                messages.success(request, f'Account created for {username}... U can now Login')
+                return redirect('login')
+            else:
+                return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    if(request.method=='POST'):
+        u_form=UserUpdateForm(request.POST,instance=request.user)
+        p_form=ProfileUpdateForm(request.POST,request.FILES,instance=request.user.profile)
+
+
+        if(u_form.is_valid() and p_form.is_valid()):
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'User Info Updated!!')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+    context={
+        'u_form':u_form,
+        'p_form':p_form
+
+    }
+    return render(request,'users/profile.html',context)
